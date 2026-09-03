370d7efabcb045c00d96ea3ef227dec1403bce75
sanjana <icodesanjana@gmail.com>
2026-09-03 22:40:49 +0530

    initial commit

---
+++ main.java
@@ -0,0 +1,16 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int num=2;
+        if (num % 2 == 0){
+            System.out.println("even");
+        }
+        else{
+            System.out.println("odd");
+        }
+
+        for(int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+    }
+}
