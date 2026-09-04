2f0b4958156b4e5d3e86ca99de2e597f99525585
sanjana <icodesanjana@gmail.com>
2026-09-04 22:52:35 +0530

    adding basic jave programs

---
+++ input.java
@@ -0,0 +1,31 @@
+public class input {
+    public static void main(String[] args) {
+    
+        int a = 25, b = 40;
+
+        if(a>b) {
+            System.out.println(a + "is greater");
+        }
+        else{
+            System.out.println(b+"b is greater");
+        }
+        // to check the number
+        int n=-11;
+        if(n>0){
+            System.out.println("positive");
+        }
+        else if(n>0){
+            System.out.println("negative");
+        }
+        else{
+            System.out.println("may be given number is zero");
+        }
+
+        // to print multiply
+        int n=5;
+        for(int i=1; i<=10; i++ ){
+            System.out.println(n + "x" + i + "=" + (n * i));
+        }
+
+    }
+}
