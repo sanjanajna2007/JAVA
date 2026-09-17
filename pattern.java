696358b96d052d71437a7e3f48494e4aaa2c9433
sanjana <icodesanjana@gmail.com>
2026-09-17 23:02:54 +0530

    updating patterns

--- pattern.java
+++ pattern.java
@@ -1,20 +1,49 @@
 public class pattern {
+
     public static void main(String[] args) {
-        
-        // for(int i=0; i<4; i++){
-        //     for(int j=0; j<=i; j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
-        int n=5;
-        for(int i=0;i<n;i++){
-            for(int j=0; j<=n-i;j++){
+
+        int n = 5;
+
+        // Upper half
+        for(int i = 1; i <= n; i++) {
+
+            // Left stars
+            for(int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
+
+            // Spaces
+            for(int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // Right stars
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // Lower half
+        for(int i = n - 1; i >= 1; i--) {
+
+            // Left stars
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // Spaces
+            for(int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // Right stars
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
             System.out.println();
         }
     }
-
-    
 }
