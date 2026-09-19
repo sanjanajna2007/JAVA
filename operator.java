6493fef237526ff77d0f41fdc4f7ba3d958d2669
sanjana <icodesanjana@gmail.com>
2026-09-19 23:00:32 +0530

    added operator

---
+++ operator.java
@@ -0,0 +1,19 @@
+public class operator {
+    public static void main(String[] args) {
+    
+    int a=10;
+    int b=20;
+    System.out.println(a + b);
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a/b);
+    System.out.println(a%b);
+
+    System.out.println(a>b);
+    System.out.println(a<b);
+    System.out.println(a==b);
+    System.out.println(a!=b);
+    }
+
+    
+}
