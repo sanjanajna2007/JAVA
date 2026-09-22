b4de51e0cef0f4609c1728412677a329ad83f7aa
sanjana <icodesanjana@gmail.com>
2026-09-22 19:29:51 +0530

    updated the file

--- iteration.java
+++ iteration.java
@@ -2,20 +2,32 @@ import java.util.Scanner;
 public class iteration {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter a number ");
-        int age = sc.nextInt();
+        // System.out.println("enter a number ");
+        // int age = sc.nextInt();
 
-        if(age>=18){
-            System.out.println("eligible to vote");
-            System.out.println("thank you for voting");
+        // if(age>=18){
+        //     System.out.println("eligible to vote");
+        //     System.out.println("thank you for voting");
+        // }
+        // else{
+        //     System.out.println("not eligible to vote");
+        //     System.out.println("come back next time");
+        // }
+
+    while(true){
+        System.out.println("enter a marks");
+        int marks = sc.nextInt();
+
+        if (marks >= 50 && marks <= 100){
+            System.out.println("you are passed");
         }
         else{
-            System.out.println("not eligible to vote");
-            System.out.println("come back next time");
+            System.out.println("congratss you failed");
         }
-       
         
-        sc.close();
+    }
+   
+    
 
 
         
